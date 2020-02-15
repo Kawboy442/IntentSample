f76c2dfb32fe5a99521fba3e160370724be69cca
R.Ito <kawboy442@gmail.com>
2020-02-15 11:12:46 +0900

    feat: edit Activity

--- app/src/main/java/com/websarva/wings/android/intentsample/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/intentsample/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.websarva.wings.android.intentsample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ListView
 import android.widget.SimpleAdapter
+import androidx.core.content.ContextCompat.startActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -44,10 +46,34 @@ class MainActivity : AppCompatActivity() {
         val to = intArrayOf(android.R.id.text1, android.R.id.text2)
 
         val adapter =
-            SimpleAdapter(applicationContext, menuList, android.R.layout.simple_list_item_2, from, to)
+            SimpleAdapter(
+                applicationContext,
+                menuList,
+                android.R.layout.simple_list_item_2,
+                from,
+                to
+            )
 
         lvMenu.adapter = adapter
 
+        lvMenu.onItemClickListener = ListItemClickListener()
+    }
+
+    private inner class ListItemClickListener : AdapterView.OnItemClickListener {
+        override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+
+            val item = parent.getItemAtPosition(position) as MutableMap<String, String>
+
+            val menuName = item["name"]
+            val menuPrice = item["price"]
+
+            val intent = Intent(applicationContext, MenuThanksActivity::class.java)
+
+            intent.putExtra("menuName", menuName)
+            intent.putExtra("menuPrice", menuPrice)
+
+            startActivity(intent)
+        }
     }
 
 }
